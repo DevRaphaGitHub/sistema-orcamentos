561888be67f038fba82e1ed3b55c7aade87d851b
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-12-08 11:35:23 -0300

    Configurado MongoDB

---
+++ README.md
@@ -0,0 +1,18 @@
+Criar o arquivo package.json
+### npm init
+
+Instalando automaticamente as dependências
+### npm install
+
+Express
+### npm install express
+
+Nodemon
+### npm install -g nodemon
+### nodemon api/app.js
+
+Mongoose (Conexão com MongoDB)
+### npm install --save mongoose
+
+MongoDB
+### npm install mongodb --save
--- api/app.js
+++ api/app.js
@@ -1,1 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost/sistema-orcamentos", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Conexão com MongoDB realizada com sucesso!");
+}).catch((error) => {
+  console.log("Erro: Conexão com MongoDB não foi realizada! Motivo: " + error);
+});
+
+app.post("/orcamento", async (req, res) => {
+  console.log(req.body);
+  res.send("Orçamento");
+});
+
+app.listen(3333, () => {
+  console.log("Servidor iniciado na porta 3333: http://localhost:3333");
+});
