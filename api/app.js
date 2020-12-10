fba17df9f8118210ced2cf3914e4a518f015c055
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-12-10 11:22:34 -0300

    ...

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import nodemailer from 'nodemailer';
+import cors from 'cors';
 
 import './models/Orcamento.js';
 const Orcamento = mongoose.model('Orcamento');
@@ -8,6 +9,14 @@ const Orcamento = mongoose.model('Orcamento');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
 
 mongoose.connect("mongodb://localhost/sistema_orcamentos", { 
   useNewUrlParser: true,
@@ -19,18 +28,19 @@ mongoose.connect("mongodb://localhost/sistema_orcamentos", {
 });
 
 app.post("/orcamento", async (req, res) => {
-  await Orcamento.create(req.body, (error) => {
-    if (error) return res.status(400).json({
-      error: true,
-      message: "Erro: Solicitação de orçamento não pode ser enviada!"
+    await Orcamento.create(req.body, (error) => {
+      if (error) return res.status(400).json({
+        error: true,
+        message: "Erro: Solicitação de orçamento não pode ser enviada!"
+      });
     });
 
     var transport = nodemailer.createTransport({
       host: "smtp.mailtrap.io",
       port: 2525,
       auth: {
-        user: "ec46dba2f81a69",
-        pass: "d1f0829106a6d7"
+        user: "ae6bb80768c516",
+        pass: "290a8925b8771a"
       }
     });
 
@@ -38,33 +48,30 @@ app.post("/orcamento", async (req, res) => {
     var emailTexto = 'Prezado(a)\n\n Recebi a solicitação de orçamento.\n\nEm breve será encaminhado o orçamento.\n\n';
 
     var emailSendInfo = {
-      from: "6e8f608201-efc6da@inbox.mailtrap.io",
+      from: '6e8f608201-efc6da@inbox.mailtrap.io',
       to: req.body.email,
       subject: "Recebi a solicitação de orçamento",
       text: emailTexto,
       textEncoding: emailHtml
     };
 
-    async () => {
-      transport.sendMail(emailSendInfo, (error) => {
-        if (error) return res.status(400).json({
-          error: true,
-          message: "Erro: Solicitação de orçamento não pode ser enviada!"
-        });
-  
-        return res.json({
-          error: false,
-          message: "Solicitação de orçamento enviada com sucesso!"
-        });
+    await transport.sendMail(emailSendInfo, (error) => {
+      if (error) return res.status(400).json({
+        error: true,
+        message: "Erro: Solicitação de orçamento não pode ser enviada!"
       });
-    };
+  
+      return res.json({
+        error: false,
+        message: "Solicitação de orçamento enviada com sucesso!"
+      });
+    });
 
     return res.json({
       error: false,
       message: "Solicitação de orçamento enviada com sucesso!"
     });
   });
-});
 
 app.listen(3333, () => {
   console.log("Servidor iniciado na porta 3333: http://localhost:3333");
